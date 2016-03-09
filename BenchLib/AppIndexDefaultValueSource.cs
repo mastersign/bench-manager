7b7f89b3a3d5fd63c7cfed789ff00c12753058ec
Tobias Kiertscher <tobias.kiertscher@bosch.com>
2016-03-09 15:58:40 +0100

    added empty stub for AppIndex specific default values

---
+++ BenchLib/AppIndexDefaultValueSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mastersign.Bench
+{
+    public class AppIndexDefaultValueSource : IGroupedPropertySource
+    {
+        public IGroupedPropertySource AppIndex { get; set; }
+
+        public object GetGroupValue(string group, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanGetGroupValue(string group, string name)
+        {
+            return false;
+        }
+    }
+}
