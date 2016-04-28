e231dd7cc46b7e36e8db4833f590f81c7b4297e9
Tobias Kiertscher <dev@mastersign.de>
2016-04-28 10:26:23 +0200

    improved logging message format

--- BenchLib/TaskInfoLogger.cs
+++ BenchLib/TaskInfoLogger.cs
@@ -36,19 +36,21 @@ namespace Mastersign.Bench
                 info is TaskError ? "ERROR" : "INFO",
                 info.AppId ?? "global",
                 info.Message);
+            if (info.DetailedMessage != null)
+            {
+                writer.WriteLine("[{0}] DETAIL:",
+                    info.Timestamp.ToString("yyyy-MM-dd HH-mm-ss"));
+                writer.WriteLine(info.DetailedMessage);
+            }
             var err = info as TaskError;
             if (err != null)
             {
                 if (err.Exception != null)
                 {
-                    writer.WriteLine();
+                    writer.Write("[{0}] EXCEPTION: ",
+                        info.Timestamp.ToString("yyyy-MM-dd HH-mm-ss"));
                     writer.WriteLine(err.Exception.ToString());
                 }
-            }
-            if (info.ProcessOutput != null)
-            {
-                writer.WriteLine();
-                writer.WriteLine(info.ProcessOutput);
             }
         }
     }
