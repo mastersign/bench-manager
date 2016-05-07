765f0654faccce0fd67e7e046334523569b12ff2
Tobias Kiertscher <dev@mastersign.de>
2016-05-07 12:12:33 +0200

    reformatted CLIXML output from PowerShell

--- BenchLib/DefaultExecutionHost.cs
+++ BenchLib/DefaultExecutionHost.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
+using System.Xml;
 
 namespace Mastersign.Bench
 {
@@ -29,7 +31,8 @@ namespace Mastersign.Bench
                 throw new FileNotFoundException("The executable could not be found.", exe);
             }
             var collectOutput = (monitoring & ProcessMonitoring.Output) == ProcessMonitoring.Output;
-            StringBuilder sb = null;
+            StringBuilder sbStd = null;
+            StringBuilder sbErr = null;
             var si = new ProcessStartInfo(exe, arguments);
             si.UseShellExecute = false;
             si.WorkingDirectory = cwd;
@@ -40,9 +43,10 @@ namespace Mastersign.Bench
             p.StartInfo = si;
             if (collectOutput)
             {
-                sb = new StringBuilder();
-                p.OutputDataReceived += (s, e) => sb.AppendLine(e.Data);
-                p.ErrorDataReceived += (s, e) => sb.AppendLine(e.Data);
+                sbStd = new StringBuilder();
+                p.OutputDataReceived += (s, e) => sbStd.AppendLine(e.Data);
+                sbErr = new StringBuilder();
+                p.ErrorDataReceived += (s, e) => sbErr.AppendLine(e.Data);
             }
             p.Start();
             if (collectOutput)
@@ -53,12 +57,55 @@ namespace Mastersign.Bench
             p.WaitForExit();
             if (collectOutput)
             {
-                return new ProcessExecutionResult(p.ExitCode, sb.ToString());
+                string output;
+                try
+                {
+                    output = FormatOutput(sbStd.ToString())
+                        + Environment.NewLine
+                        + FormatOutput(sbErr.ToString());
+                }
+                catch (Exception e)
+                {
+                    output = e.ToString();
+                }
+                return new ProcessExecutionResult(p.ExitCode, output);
             }
             else
             {
                 return new ProcessExecutionResult(p.ExitCode);
             }
+        }
+
+        private static string CliXmlFormatter(Match m)
+        {
+            var xml = m.Value;
+            var doc = new XmlDocument();
+            try
+            {
+                doc.LoadXml(m.Value);
+            }
+            catch (Exception) { }
+            var sb = new StringBuilder();
+            var nsm = new XmlNamespaceManager(new NameTable());
+            nsm.AddNamespace("ps", "http://schemas.microsoft.com/powershell/2004/04");
+            foreach (XmlElement e in doc.SelectNodes("/ps:Objs/ps:Obj/ps:ToString", nsm))
+            {
+                sb.AppendLine(e.InnerText);
+            }
+            return sb.ToString();
+        }
+
+        private static string FormatOutput(string str)
+        {
+            str = str.Trim();
+            var preamble = "#< CLIXML";
+            if (str.StartsWith(preamble))
+            {
+                str = str.Substring(preamble.Length).TrimStart();
+            }
+            var xmlObjsPattern = new Regex(@"\<Objs ([\s\S]*?)\<\/Objs\>");
+            str = xmlObjsPattern.Replace(str, CliXmlFormatter);
+            return str;
         }
     }
 }
