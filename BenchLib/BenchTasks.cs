29512d5383080365d82b532c44f4f0cc2fc15ee4
Tobias Kiertscher <dev@mastersign.de>
2016-05-19 09:50:07 +0200

    cleaned execution proxy generation - removed unnecessary comma - quoted app id as a precaution

--- BenchLib/BenchTasks.cs
+++ BenchLib/BenchTasks.cs
@@ -756,7 +756,7 @@ namespace Mastersign.Bench
                     var proxyPath = app.GetExecutableProxy(exePath);
                     var code = new StringBuilder();
                     code.AppendLine("@ECHO OFF");
-                    code.AppendLine(string.Format("runps Run-Adorned {0}, \"{1}\" %*", app.ID, exePath));
+                    code.AppendLine(string.Format("runps Run-Adorned \"{0}\" \"{1}\" %*", app.ID, exePath));
                     File.WriteAllText(proxyPath, code.ToString());
                 }
             }
